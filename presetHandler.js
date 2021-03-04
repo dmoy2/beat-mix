824a8bbe372d1ba97625618ec7260ebf34a4ab3c
Derek Moy <dmoy2@dons.usfca.edu>
2021-03-04 14:53:33 -0600

    Adding functions to be implemented.

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,15 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+/*  returns 404 as first array element if invalid index 
+    400 as first array element if httpVerb is not GET or PUT 
+    200 as first array element if index is valid 
+    if index valid for GET - second element in array should be preset array 
+    if index valid for PUT - save newPresetArray to index and return it as second element of array 
+*/
+const presetHandler = (httpVerb, arrayIndex, newPresetArray) => {
+    // returns array 
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,21 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = new Array(16).fill(false);
+let snares = new Array(16).fill(false);
+let hiHats = new Array(16).fill(false);
+let rideCymbals = new Array(16).fill(false);
+
+/*  toggleDrums() - flip the boolean value in the correct array at the specified index. */ 
+function toggleDrums(arrayName, arrayIndex) {
+    // do something;
+}
+
+/*  clear() - takes an array name string and sets all values in the correct array to false. */
+function clear(arrayName) {
+    // do something;
+}
+
+/* invert() - takes an array name string and flips the boolean value of all elements in the correct array. */
+function invert(arrayName) {
+    // do something;
+}
 
