9a947323e3a89f3246e4315704893bd38b26c426
Derek Moy <dmoy2@dons.usfca.edu>
2021-03-04 17:22:49 -0600

    33 tests passing. Working on GET request handler.

--- presetHandler.js
+++ presetHandler.js
@@ -9,8 +9,45 @@ const presets = require('./presets');
     if index valid for PUT - save newPresetArray to index and return it as second element of array 
 */
 const presetHandler = (httpVerb, arrayIndex, newPresetArray) => {
+    let result = [];
+
+    // validate httpVerb 
+    if(httpVerb !== 'GET' && httpVerb !== 'PUT') {
+        result.push(400);
+    } 
+    
+    // handle GET requests 
+    if (httpVerb === 'GET') {
+        // validate arrayIndex
+        if(!isArrayIndexValid(arrayIndex)) {
+            result.push(404);
+        } else {
+            result.push(200);
+        }
+
+        // handle successful GET requests 
+        result.push(presets);
+    }
+    
+    // validating arrayIndex 
+
+
     // returns array 
+    // console.log(result);
+    return result;
 };
+
+// validate array index 
+function isArrayIndexValid(arrayIndex) {
+    if(arrayIndex === null){
+        return false;
+    } else if(arrayIndex < 0 || arrayIndex > 15) {
+        return false;
+    } else {
+        return true;
+    }
+}   
+
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
